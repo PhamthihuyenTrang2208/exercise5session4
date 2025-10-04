b0c7710eacb56eb32a2905d00eb8f98bf2c5399e
PhamthihuyenTrang2208 <huyentrang22ori@gmail.com>
2025-10-04 21:19:38 +0900

    Add files via upload

--- studentlist_students.sql
+++ studentlist_students.sql
@@ -16,28 +16,28 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `courses`
+-- Table structure for table `students`
 --
 
-DROP TABLE IF EXISTS `courses`;
+DROP TABLE IF EXISTS `students`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `courses` (
-  `courseID` int NOT NULL AUTO_INCREMENT,
-  `courseName` varchar(100) NOT NULL,
-  `instructor` varchar(50) NOT NULL,
-  PRIMARY KEY (`courseID`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `students` (
+  `StudentID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `Major` varchar(50) NOT NULL,
+  PRIMARY KEY (`StudentID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `courses`
+-- Dumping data for table `students`
 --
 
-LOCK TABLES `courses` WRITE;
-/*!40000 ALTER TABLE `courses` DISABLE KEYS */;
-INSERT INTO `courses` VALUES (1,'Database System','Mr.Sato'),(2,'Advanced Mathematics','Mr.Suzuki');
-/*!40000 ALTER TABLE `courses` ENABLE KEYS */;
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (2,'Bob','Mathematics'),(3,'Charlie','Engineering'),(4,'Tom','Math');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +49,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-04 21:18:37
+-- Dump completed on 2025-10-04 21:18:36
